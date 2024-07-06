97c4236cc29dc0f2a9d28c4468e99401b1a2bd2a
ChangeHow <23733347+ChangeHow@users.noreply.github.com>
2024-07-06 22:41:27 +0800

    feat: add log util to bootstrap
    to allow running script directly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-source $(pwd)/scripts/utils/log.sh
+
+# 定义一些颜色代码
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+color_echo() {
+  COLOR=$1
+  shift
+  echo "${!COLOR}$@${NC}"
+}
+
+prefix_log() {
+  local prefix=${2:-log}
+  color_echo BLUE "[$prefix] $1"
+}
 
 set -e
 
